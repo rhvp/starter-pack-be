2f7a6b59b0c31945b7a2d61b2bdccc9e3bdc6e42
Abdul Ajoge <bugsabdul@yahoo.com>
2020-08-24 12:27:40 +0100

    add pre-heroku commit files

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,7 +45,7 @@ exports.login = async(req, res, next) => {
             ),
             httpOnly: true
         };
-        if(process.env.NODE_ENV === 'production') cookie_Options.secure = true;
+        if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
         res.cookie('jwt', token, cookieOptions);
         user.password = undefined;
         res.status(200).json({
