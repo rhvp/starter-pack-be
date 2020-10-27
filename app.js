48b14c03cf3f40d6f184870842cd6527a187c24a
Abdul Ajoge <bugsabdul@yahoo.com>
2020-10-27 14:10:12 +0100

    increase request limit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(morgan('short'));
 app.use(helmet());
 
 const limiter = rateLimit({
-    max: 100,
+    max: 500,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!'
 });
