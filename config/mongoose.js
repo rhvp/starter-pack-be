72d7741c3c2d9b3d7de5dfd8782e37d70c93fa30
Abdul Ajoge <bugsabdul@yahoo.com>
2020-10-28 09:35:16 +0100

    switch to live

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_TEST_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, autoIndex: true})
+mongoose.connect(process.env.MONGO_LIVE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, autoIndex: true})
 .then(
     res=>console.log('DB Connection Success'))
 .catch(err=>{
